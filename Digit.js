ec142f8c0ea935fde83e089f15152781eb2489ce
md.nurul <nurulislam8433@gmail.com>
2024-12-12 22:55:15 +0600

    'some'

--- Digit.js
+++ Digit.js
@@ -1,28 +1,30 @@
 const digits=[15,12,17,11,31 ,60,19,28];
 
 
-/*for(let digit in digits){
+for(let digit in digits){
 	const first=digits[digit];
 	const key2=Number(digit)+1;
 	const last=digits[key2];
 	
-	console.log(first);
+	//console.log(first);
 	//console.log(last);
-	if(first>last){
+	if(first>last ){
 	digits[digit]=last;
     digits[key2]=first;	
 	}
 } 
 
-	console.log(digits);*/
+	console.log(digits);
 	
 	
- for(let i=0;i<digits.length;i++){
+ /*for(let i=0;i<digits.length;i++){
 	 const first=digits[i];
 	 const key2=digits;
 	
+	if(first,key2){
+	 return a-b
+		
+	}
 	 
-	 
-	 
-	 console.log(key2);
- }
+	console.log(digits); 
+ } */
