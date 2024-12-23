3218fb43191e96ad20851fe73668650e2e022739
md.nurul <nurulislam8433@gmail.com>
2024-12-23 23:17:39 +0600

    work

--- ResulSheet.js
+++ ResulSheet.js
@@ -9,15 +9,16 @@
 const studant={
 	bangla:70,
 	english:60,
-	arabic:40,
+	arabic:50,
 	
 };
 
 for(let studants in studant  ){
 	
 const Subjectnumber=studant[studants];
+const GPA=numberTopoint(Subjectnumber);
 	
-	console.log(Subjectnumber);
+	console.log(GPA);
 	
 };
 
@@ -26,20 +27,29 @@ function numberTopoint(marks){
 	let point=0;
 	
 	if(marks>=80){
-		point=5.00;
+		point=5.00
 	}
 
- else if(marks>=70){
-	 point:4.00;
+   else if(marks>=70){
+	 point=4.00;
  }
-  else if(marks>=60){
-	 point:3.00;
+   else if(marks>=60){
+	 point=3.00;
  }
- else if(marks>=50){
-	 point:2.00;
+   else if(marks>=50){
+	 point=2.00;
  }
- return point;
+     return point;
 	
 };
 
-const GPA=numberTopoint(marks)
+   const subject =Object.keys(studant);
+   const subjectLength=subject.length;
+   
+   console.log(subject);
+
+
+
+
+
+//console.log(GPA);
