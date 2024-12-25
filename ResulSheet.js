7056f7f770b0354b2a8e21f969a5b41237f40be1
md.nurul <nurulislam8433@gmail.com>
2024-12-25 12:58:44 +0600

    'work'

--- ResulSheet.js
+++ ResulSheet.js
@@ -7,27 +7,29 @@
 */
 
 const studant={
-	bangla:70,
+	bangla:80,
 	english:60,
-	arabic:50,
+	arabic:80,
 	
 };
 
 for(let studants in studant  ){
 	
 const Subjectnumber=studant[studants];
-const GPA=numberTopoint(Subjectnumber);
+const grade=subjectToGrade(Subjectnumber)
 	
-	console.log(GPA);
+	console.log(grade);
 	
 };
+
+function subjectToGrade(subects){
 
 
 function numberTopoint(marks){
 	let point=0;
 	
 	if(marks>=80){
-		point=5.00
+		point=5.00;
 	}
 
    else if(marks>=70){
@@ -43,13 +45,44 @@ function numberTopoint(marks){
 	
 };
 
+ 
+ let points=0;
+for(let subject in studant ){
+	const subjectName=studant[subject];
+const X=numberTopoint(subjectName);
+	points=points+X;
+};
+
+	//console.log(points);
+
+
    const subject =Object.keys(studant);
    const subjectLength=subject.length;
+   const GPA=points/subjectLength;
    
-   console.log(subject);
+  //console.log(GPA);
 
+function pointToGread(point){
+	let grade='';
+	
+	if(point>=5.00){
+		grade="A+";
+	}
+	else if(point>=4.00){
+		grade="B+";
+	}
+	else if(point>=3.00){
+		grade="C+";
+	}
+	else if(point>=2.00){
+		grade="D";
+	}
+	
+	return grade;
+}
 
+const Grade=pointToGread(GPA);
+//console.log(Grade);
+  return Grade;
 
-
-
-//console.log(GPA);
+};
