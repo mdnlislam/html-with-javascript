c77506f91d8142edad86fa8c6bf3d7875bba6b56
md.nurul <nurulislam8433@gmail.com>
2025-01-04 23:12:05 +0600

    'calculator'

--- Kiln_Department.js
+++ Kiln_Department.js
@@ -1,26 +1,25 @@
-const mouseover1=document.querySelector('.tiles_picture1');
-
-const mouseover2=document.querySelector('.tiles_picture2');
 
 
-const wall_tiles=document.querySelector('.wall_tiles');
-const out_put=document.querySelector('.out_put');
+const mouseover1 = document.querySelector('.tiles_picture1');
+const mouseover2 = document.querySelector('.tiles_picture2');
 
-const floor_tiles=document.querySelector('.floor_tiles');
+const wall_tiles = document.querySelector('.wall_tiles');
+const out_put = document.querySelector('.out_put');
 
+const floor_tiles = document.querySelector('.floor_tiles');
 
-mouseover1.addEventListener('mouseover',function(){
+mouseover1.addEventListener('mouseover', function () {
 	
-	out_put.innerHTML=wall_tiles.innerHTML;
-	mouseover1.style.display='block';
-	
+	if(wall_tiles && out_put){
+		out_put.innerHTML = wall_tiles.innerHTML;
+    out_put.style.display = 'block'; 
+	out_put.style.color='red';
+	}
+    
 });
 
-mouseover1.addEventListener('mouseout',function(){
-	out_put.style.display='none';
-	
+mouseover1.addEventListener('mouseout', function () {
+    out_put.style.display = 'none';
 });
 
-
-
-console.log(mouseover2,mouseover1,wall_tiles,floor_tiles);
+//console.log(mouseover2, mouseover1, wall_tiles, floor_tiles);
